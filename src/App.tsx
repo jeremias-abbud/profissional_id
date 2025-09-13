e3a609646c4cf9c55d17e4744fd9646b0c0b3c64
Jeremias <jeremias.abbud@gmail.com>
2025-09-13 20:16:00 -0300

    ajustando css

--- src/App.tsx
+++ src/App.tsx
@@ -11,56 +11,54 @@ import AdminPanel from "./pages/AdminPanel";
 const queryClient = new QueryClient();
 
 const App = () => {
-  const [uploadedImages, setUploadedImages] = useState<string[]>([]);
+  const [uploadedImages, setUploadedImages] = useState<string[]>([]);
 
-  useEffect(() => {
-    // Carregar imagens do localStorage
-    const savedImages = localStorage.getItem('profissionalid-carousel-images');
-    if (savedImages) {
-      setUploadedImages(JSON.parse(savedImages));
-    }
+  useEffect(() => {
+    const savedImages = localStorage.getItem('profissionalid-carousel-images');
+    if (savedImages) {
+      setUploadedImages(JSON.parse(savedImages));
+    }
 
-    // Escutar mudanças no localStorage (quando admin adiciona imagens)
-    const handleStorageChange = () => {
-      const savedImages = localStorage.getItem('profissionalid-carousel-images');
-      if (savedImages) {
-        setUploadedImages(JSON.parse(savedImages));
-      }
-    };
+    const handleStorageChange = () => {
+      const savedImages = localStorage.getItem('profissionalid-carousel-images');
+      if (savedImages) {
+        setUploadedImages(JSON.parse(savedImages));
+      }
+    };
 
-    window.addEventListener('storage', handleStorageChange);
-    
-    // Também escutar mudanças na mesma aba
-    const interval = setInterval(() => {
-      const savedImages = localStorage.getItem('profissionalid-carousel-images');
-      const currentImages = JSON.stringify(uploadedImages);
-      if (savedImages && savedImages !== currentImages) {
-        setUploadedImages(JSON.parse(savedImages));
-      }
-    }, 1000);
+    window.addEventListener('storage', handleStorageChange);
+    
+    const interval = setInterval(() => {
+      const savedImages = localStorage.getItem('profissionalid-carousel-images');
+      const currentImages = JSON.stringify(uploadedImages);
+      if (savedImages && savedImages !== currentImages) {
+        setUploadedImages(JSON.parse(savedImages));
+      }
+    }, 1000);
 
-    return () => {
-      window.removeEventListener('storage', handleStorageChange);
-      clearInterval(interval);
-    };
-  }, [uploadedImages]);
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+      clearInterval(interval);
+    };
+  }, [uploadedImages]);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index uploadedImages={uploadedImages} />} />
-            <Route path="/admin" element={<AdminPanel />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
+  return (
+    <div className="overflow-x-hidden"> {/* Adicione esta linha */}
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index uploadedImages={uploadedImages} />} />
+              <Route path="/admin" element={<AdminPanel />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </div>
+  );
 };
 
 export default App;
